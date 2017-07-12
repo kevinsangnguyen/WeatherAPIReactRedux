630578de0fed758a9af19ea5b8dc4e4a11301060
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-07-11 19:55:28 -0700

    setup axios for get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '3ea6e3caa9f8760f2641186cf3e179cd';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
-  //returns action
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
   return {
     type: FETCH_WEATHER,
+    payload: request
   };
 }
